faa178353861087c3e1fd3f8ec52e641814b0a58
gabrielqueiroz <gabriel.fatec@hotmail.com>
2017-04-25 00:04:48 -0300

    Log bean names on Application start

--- src/main/example/Application.kt
+++ src/main/example/Application.kt
@@ -1,7 +1,9 @@
 package example
 
+import org.slf4j.LoggerFactory
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import java.util.*
 
 /**
  * Created by gabrielqueiroz on 4/24/17.
@@ -9,6 +11,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 @SpringBootApplication
 open class Application
 
-fun main(args: Array<String>) {
-    SpringApplication.run(Application::class.java, *args)
+fun main (args: Array<String>) {
+    val context = SpringApplication.run(Application::class.java, *args)
+    val log = LoggerFactory.getLogger("Application")
+    Arrays.stream(context.getBeanDefinitionNames()).forEach { bean -> log.info("BEAN: {}", bean) }
 }
+
