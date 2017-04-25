0214045777e593818bf337e1398284aa8188c00e
gabrielqueiroz <gabriel.fatec@hotmail.com>
2017-04-25 00:23:24 -0300

    Sort bean name on application main

--- src/main/example/Application.kt
+++ src/main/example/Application.kt
@@ -4,6 +4,7 @@ import org.slf4j.LoggerFactory
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import java.util.*
+import java.util.Arrays.sort
 
 /**
  * Created by gabrielqueiroz on 4/24/17.
@@ -13,7 +14,8 @@ open class Application
 
 fun main (args: Array<String>) {
     val context = SpringApplication.run(Application::class.java, *args)
-    val log = LoggerFactory.getLogger("Application")
-    Arrays.stream(context.getBeanDefinitionNames()).forEach { bean -> log.info("BEAN: {}", bean) }
+    val beans   = context.getBeanDefinitionNames()
+    sort(beans)
+    Arrays.stream(beans).forEach{ bean -> LoggerFactory.getLogger("Application").info("BEAN: {}", bean) }
 }
 
